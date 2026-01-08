4d32acf7801fb63074f0656460db3f611c2e6b9a
K.H.Yeh <koco8654@gmail.com>
2026-01-08 19:45:36 +0800

    first commit

---
+++ src/main/java/com/PetGuardianApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetGuardianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetGuardianApplication.class, args);
+	}
+
+}
