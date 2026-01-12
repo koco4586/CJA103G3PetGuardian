e1297fa8f1c76ac1eabf33589d2bafd59666aa54
Willson <goal151234560@gmail.com>
2026-01-12 14:17:27 +0800

    sitterVO test

---
+++ src/main/java/com/sitter/sitterVO.java
@@ -0,0 +1,16 @@
+package com.sitter;
+
+import java.time.LocalDateTime;
+
+public class sitterVO {
+	private Integer sitterId;
+	private Integer memId;
+	private String sitterName;
+	private String sitterAdd;
+	private Integer defaultPrice;
+	private LocalDateTime sitterCreatedAt;
+	private String serviceTime;
+	private Integer sitterRatingCount;
+	private Integer sitterStarCount;
+	
+}
