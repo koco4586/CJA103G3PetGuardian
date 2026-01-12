9f913d3c1487209f727da5b3a567b2e7074a512d
Willson <goal151234560@gmail.com>
2026-01-12 17:51:41 +0800

    PUSH VO2

--- src/main/java/com/sitter/model/sitterVO.java
+++ src/main/java/com/sitter/model/sitterVO.java
@@ -5,8 +5,17 @@ import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class sitterVO implements Serializable {
@@ -18,19 +27,27 @@ public class sitterVO implements Serializable {
 	
 	@Id //@Id代表這個屬性是這個Entity的唯一識別屬性，並且對映到Table的主鍵
 	@Column(name = "sitterId")
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //@GeneratedValue的generator屬性指定要用哪個generator //【strategy的GenerationType, 有四種值: AUTO, IDENTITY, SEQUENCE, TABLE】 
 	private Integer sitterId; //保姆編號
 	
 	@OneToOne
-	@Column(name ="memId", unique = true)  //會員編號
+	@JoinColumn(name ="memId", unique = true)  //會員編號
 	private Integer memId;
 	
 	@Column(name="sitterName")
 	private String sitterName; //保姆姓名
 	
 	@Column(name="sitterAdd")
+	@NotEmpty(message ="服務地址不能空白")
+	@Size(min = 5, max = 100, message = "服務地址長度必須介於5到100字元")
+	@Pattern(regexp = "^[\\u4e00-\\u9fa5a-zA-Z0-9\\s]+$",
+			message = "服務地址只能包含中文、英文、數字或空格")
 	private String sitterAdd;  //服務地址
 	
 	@Column(name="defaultPrice")
+	@NotNull(message="規範價格: 請勿空白")
+	@DecimalMin(value = "400", message = "規範價格: 不能小於{value}")
+	@DecimalMax(value = "1000", message = "規範價格: 不能超過{value}")
 	private Integer defaultPrice; //規範價格
 	
 	@Column(name="sitterCreatedAt")
@@ -62,8 +79,11 @@ public class sitterVO implements Serializable {
 		this.memId = memId;
 	}
 	public String getSitterName() {
+		//等會員VO做好
+//	    return member != null ? member.getMemName() : null;
 		return sitterName;
 	}
+
 	public void setSitterName(String sitterName) {
 		this.sitterName = sitterName;
 	}
@@ -83,6 +103,9 @@ public class sitterVO implements Serializable {
 		return sitterCreatedAt;
 	}
 	
+	public void setSitterCreatedAt(LocalDateTime sitterCreatedAt) {
+		this.sitterCreatedAt = sitterCreatedAt;
+	}
 	public Byte getSitterStatus() {
 		return sitterStatus;
 	}
