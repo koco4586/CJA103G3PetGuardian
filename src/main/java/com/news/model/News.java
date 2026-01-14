8a0adc40fa47a27374043531086a2aac9fad8570
Lynn Hou <sss95203@gmail.com>
2026-01-15 00:49:42 +0800

    newsvo update

--- src/main/java/com/news/model/News.java
+++ src/main/java/com/news/model/News.java
@@ -4,11 +4,7 @@ import java.time.LocalDateTime;
 
 import org.springframework.data.annotation.Id;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
