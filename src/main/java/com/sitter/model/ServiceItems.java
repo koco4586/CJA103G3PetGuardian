11a3e88b171fad9c120e65476e644861e8b5517a
Willson <goal151234560@gmail.com>
2026-01-14 20:29:36 +0800

    Test Push000

---
+++ src/main/java/com/sitter/model/ServiceItems.java
@@ -0,0 +1,45 @@
+package com.sitter.model;
+
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity 
+@Table(name ="service_items")
+public class ServiceItems implements Serializable{
+	
+	public ServiceItems() {
+		super();
+	}
+	
+	@Id
+	@Column(name = "service_item_Id", updatable = false, nullable = false)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer serviceItemId;
+	
+	@Column(name = "service_type")
+	private String serviceType;
+
+	public Integer getServiceItemId() {
+		return serviceItemId;
+	}
+
+	public void setServiceItemId(Integer serviceItemId) {
+		this.serviceItemId = serviceItemId;
+	}
+
+	public String getServiceType() {
+		return serviceType;
+	}
+
+	public void setServiceType(String serviceType) {
+		this.serviceType = serviceType;
+	}		
+	
+}
