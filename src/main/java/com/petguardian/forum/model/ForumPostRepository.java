4a864e4ccf9f247ddfd7613677c6fb65de773980
Sam Chen <cyc881010@icloud.com>
2026-01-19 17:17:38 +0800

    新增論壇貼文留言功能，包含留言新增、顯示及錯誤處理邏輯

--- src/main/java/com/petguardian/forum/model/ForumPostRepository.java
+++ src/main/java/com/petguardian/forum/model/ForumPostRepository.java
@@ -15,9 +15,9 @@ public interface ForumPostRepository extends JpaRepository<ForumPostVO, Integer>
 	// 	Spring 會自動解析為：postStatus = 1 AND postTitle LIKE %...% ORDER BY postId DESC
 	//	List<ForumPostVO> findByPostStatusAndPostTitleContainingOrderByPostIdDesc(Integer postStatus, String postTitle);
 	
-	//	後台用，顯示已被(檢舉下架)的文章
+	//	後台用，顯示已處理(檢舉下架)的文章
 	@Query(value = "select p from ForumPostVO p join fetch p.forum f where p.postStatus = 0")
-	public List<ForumPostVO> getAllReportedPost();
+	public List<ForumPostVO> getAllHandledPosts();
 	
 	//	從討論區id拿到該討論區所有啟用中文章
 	@Query(value = "select p from ForumPostVO p where p.forum.forumId = :forumId and p.postStatus = 1 order by p.postId desc")
