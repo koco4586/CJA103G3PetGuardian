836a757209ae45ac63b4e706413da4b498e56683
wanzhi <linwanzhi163@gmail.com>
2026-01-19 09:49:08 +0800

    model update

---
+++ src/main/java/com/petguardian/returnorder/model/ReturnOrderVO.java
@@ -0,0 +1,35 @@
+package com.petguardian.returnorder.model;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "return_order")
+@Setter @Getter
+public class ReturnOrderVO {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "return_id")
+    private Integer returnId;
+
+    @Column(name = "order_id", nullable = false)
+    private Integer orderId;
+
+    @Column(name = "apply_time", nullable = false, insertable = false, updatable = false)
+    private LocalDateTime applyTime;
+
+    @Column(name = "return_reason", length = 500, nullable = false)
+    private String returnReason;
+
+    @Column(name = "refund_amount", nullable = false, columnDefinition = "INT UNSIGNED")
+    private Integer refundAmount;
+
+    @Column(name = "return_status", nullable = false)
+    private Integer returnStatus = 0; // 0:審核中 1:退貨通過 2:退貨失敗
+
+}
