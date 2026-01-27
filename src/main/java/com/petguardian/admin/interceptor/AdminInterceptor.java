9b0a70c0170de3984d797fcbdf4aef6de6f80fda
teresa <happy19970305@gmail.com>
2026-01-27 19:18:31 +0800

    Finish Admin Interceptor.

---
+++ src/main/java/com/petguardian/admin/interceptor/AdminInterceptor.java
@@ -0,0 +1,35 @@
+package com.petguardian.admin.interceptor;
+
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@Component
+public class AdminInterceptor implements HandlerInterceptor{
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull Object handler)
+            throws Exception {
+		
+		HttpSession session = request.getSession(false);
+		
+		Integer admId = (session != null) ? (Integer)session.getAttribute("admId") : null;
+		
+		if(admId != null){
+            return true;
+        }
+
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);//401
+
+        response.setContentType("application/json;charset=UTF-8");
+
+        response.getWriter().write("{\"message\":\"請先登入\",\"code\":\"UNAUTHORIZED\"}");
+
+        return false;
+    }
+
+}
