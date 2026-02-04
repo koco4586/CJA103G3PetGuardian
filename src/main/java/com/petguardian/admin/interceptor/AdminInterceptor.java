2a97300adc587f08f6588bbe113c8f0bc756f212
teresa <happy19970305@gmail.com>
2026-02-04 20:20:07 +0800

    Finish Member and Admin Page CSS StyleSheet.

--- src/main/java/com/petguardian/admin/interceptor/AdminInterceptor.java
+++ src/main/java/com/petguardian/admin/interceptor/AdminInterceptor.java
@@ -30,11 +30,12 @@ public class AdminInterceptor implements HandlerInterceptor {
 //			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 //			response.setContentType("application/json;charset=UTF-8");
 //			response.getWriter().write("{\"message\":\"請先登入\",\"code\":\"UNAUTHORIZED\"}");
-//			
+            
+			//有登入畫面後，上面三行改成下面這行,這行只是在 response 物件中「設置」了重定向的標頭,不會直接飛走
 			response.sendRedirect(request.getContextPath() + "/html/backend/admin/admin_login.html");  //request.getContextPath():應用程式部署的路徑前綴,這裡取到的是 "" (空字串)
 
 			
-			return false;
+			return false;   //這行執行時拒絕http request請求,整個請求處理流程結束後,response才被發送給瀏覽器,瀏覽器收到重定向的標頭才"真正跳轉"
 		}
 
 		return true;
